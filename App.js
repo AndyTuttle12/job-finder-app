3c3e8dc3a6b66a4b51df61b475343307f4aa60d1
Andy Tuttle <andytuttle12@gmail.com>
2017-09-08 23:29:17 -0400

    Staring new app for job searches.

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckSreen from './screens/DeckScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MapNavigator = TabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckSreen }
+    });
+    
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckSreen }
+        },{
+          swipeEnabled: false,
+          lazyLoad: true,
+          animationEnabled: false
+        })
+      }
+    });
+
+    return (
+        <MainNavigator navigation={this.props.navigation} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
