03392b1e69434f364b6b282b7fe25810052fd633
Andy Tuttle <andytuttle12@gmail.com>
2017-09-09 22:13:35 -0400

    Added more navigation and the welcome flow.

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckSreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
-    const MapNavigator = TabNavigator({
-      map: { screen: MapScreen },
-      deck: { screen: DeckSreen }
-    });
-    
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
       auth: { screen: AuthScreen },
       main: {
         screen: TabNavigator({
           map: { screen: MapScreen },
-          deck: { screen: DeckSreen }
+          deck: { screen: DeckSreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            },{
+              swipeEnabled: false,
+              lazyLoad: true,
+              animationEnabled: false
+            })
+          }
         },{
           swipeEnabled: false,
           lazyLoad: true,
