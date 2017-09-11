d687f4acc45863e16fe114fb0c3237b65fdef09a
Andy Tuttle <andytuttle12@gmail.com>
2017-09-10 20:30:07 -0400

    Added redux and started auth with Facebook.

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
-
+import { Provider } from 'react-redux';
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -37,7 +38,9 @@ export default class App extends React.Component {
     });
 
     return (
+      <Provider store={store}>
         <MainNavigator navigation={this.props.navigation} />
+      </Provider>
     );
   }
 }
