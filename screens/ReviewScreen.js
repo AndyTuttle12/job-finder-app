35581dd7ec10fae1405c741ad53bfa12fc90e4e9
Andy Tuttle <andytuttle12@gmail.com>
2017-09-17 20:25:36 -0400

    Added mapView and card styles to review screen.

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Platform, ScrollView, Linking } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -21,10 +22,33 @@ class ReviewScreen extends Component {
 
     renderLikedJobs() {
         return this.props.likedJobs.map(job => {
-            const { company, formattedRelativeTime, url } = job;
+            const {
+                company,
+                formattedRelativeTime,
+                url,
+                longitude,
+                latitude,
+                jobtitle,
+                jobkey
+            } = job;
+            const initialRegion = {
+                longitude,
+                latitude,
+                longitudeDelta: 0.045,
+                latitudeDelta: 0.02
+            };
             return (
-                <Card>
+                <Card
+                    title={jobtitle}
+                    key={jobkey}
+                >
                     <View style={{ height: 200 }}>
+                        <MapView
+                            style={{ flex: 1 }}
+                            cacheEnabled={Platform.OS === 'android'}
+                            scrollEnabled={false}
+                            initialRegion={initialRegion}
+                        />
                         <View style={styles.detailWrapper}>
                             <Text style={styles.italics}>{company}</Text>
                             <Text style={styles.italics}>{formattedRelativeTime}</Text>
@@ -51,6 +75,7 @@ class ReviewScreen extends Component {
 
 const styles = {
     detailWrapper: {
+        marginTop: 10,
         marginBottom: 10,
         flexDirection: 'row',
         justifyContent: 'space-around'
